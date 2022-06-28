c5de4cf362959405f4658313230acc2d0b6d1f36
Abdenour Touat <61945965+abdenour-progdes@users.noreply.github.com>
2022-06-28 20:51:50 +0100

    add the banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <aside className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0 max-w-7xl mx-auto">
+      <div className="px-10 space-y-5">
+        <h1 className="text-6xl max-w-xl font-serif">
+          <span className="underline decoration-black decoration-4">
+            Abdennour's blog
+          </span>{" "}
+          is a place to write, read blogs
+        </h1>
+        <h2>It's easy and free to share your thoughts with millions people.</h2>
+      </div>
+      <img
+        className="hidden md:inline-flex h-32 lg:h-full"
+        src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+        alt="logo"
+      />{" "}
+    </aside>
+  );
+};
+
+export default Banner;
